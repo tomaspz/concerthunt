3359cbf747b92a1105c03a3a8cb1abe18b782ff7
Eshi44 <e.hameliervolino@gmail.com>
2020-03-20 13:25:46 -0400

    do not merge

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,22 @@ console.log("hello world");
 //added document.ready function - Em
 
 $(document).ready(function() {
-  
   // bandsintown API Key
   const bandsAPIKey = "7a94704114b40126fda0059aab05bb1c";
   const lastFmAPIKey = "f73c832fa45f573c5aa8ef6885d8fab3";
 
   // create card for searched artist
   function createCard(artist) {
-    return `<div class='large-3 columns'><div class='card'><img class='card-img' src=${artist.image_url} alt='header' /><div class='card-info'><h1 class='card-title'>${artist.name}</h1><div class='card-icon'>${artist.icon}</div><p class='card-author'>${artist.genre}</p><p class='card-stats'>6 <img src="https://placehold.it/20" alt="hi" /> 6 <img src="https://placehold.it/20" alt="hi" /></p></div></div></div>`
+    return `<div class='large-3 columns'><div class='card'><img class='card-img' src=${artist.image_url} alt='header' /><div class='card-info'><h1 class='card-title'>${artist.name}</h1><div class='card-icon'>${artist.icon}</div><p class='card-author'>${artist.genre}</p><p class='card-stats'>6 <img src="https://placehold.it/20" alt="hi" /> 6 <img src="https://placehold.it/20" alt="hi" /></p></div></div></div>`;
   }
 
   function artistQueryLastFM(artist) {
-
     var queryLastFMURL =
-      "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" + artist + "&api_key=" + lastFmAPIKey + "&format=json";
+      "https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" +
+      artist +
+      "&api_key=" +
+      lastFmAPIKey +
+      "&format=json";
 
     $.ajax({
       url: queryLastFMURL,
@@ -26,7 +28,6 @@ $(document).ready(function() {
   }
 
   function artistQueryBIT(artist) {
-
     var queryBandsURL =
       "https://rest.bandsintown.com/artists/" +
       artist +
@@ -41,12 +42,12 @@ $(document).ready(function() {
     });
   }
 
-  function concertQueryBIT(artist){
+  function concertQueryBIT(artist) {
     var queryConcertURL =
-    "https://rest.bandsintown.com/artists/" +
-    artist +
-    "/events?app_id=" +
-    bandsAPIKey;
+      "https://rest.bandsintown.com/artists/" +
+      artist +
+      "/events?app_id=" +
+      bandsAPIKey;
 
     $.ajax({
       url: queryConcertURL,
@@ -57,7 +58,6 @@ $(document).ready(function() {
   }
 
   $("#searchBtn").on("click", function(event) {
-
     event.preventDefault();
 
     // get artist name from input field
@@ -67,6 +67,5 @@ $(document).ready(function() {
     artistQueryBIT(artist);
     artistQueryLastFM(artist);
     concertQueryBIT(artist);
-
   });
 });
